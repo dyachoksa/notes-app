d8a619ea13694fe413a54b22c2b13d287e609375
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-04-06 18:25:06 +0300

    Fixed UI

--- notes_app/ui.py
+++ notes_app/ui.py
@@ -5,32 +5,46 @@ class ApplicationFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.SetSize(800, 600)
+        self.SetMinSize(wx.Size(600, 500))
+
         panel = wx.Panel(self)
 
-        self.notesListWidget = wx.ListCtrl(panel, style=wx.LC_REPORT)
-        self.notesListWidget.AppendColumn("ID")
-        self.notesListWidget.AppendColumn("Title")
+        self.notes_list_widget = wx.ListBox(panel, style=wx.LB_SINGLE)
+        self.notes_list_widget.SetSelection(wx.NOT_FOUND)
 
-        self.textWidget = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        # test item
+        self.notes_list_widget.Append("My Note")
 
-        self.deleteBtn = wx.Button(panel, label="Delete")
-        self.saveBtn = wx.Button(panel, label="Save")
+        notes_sizer = wx.BoxSizer(wx.VERTICAL)
+        notes_sizer.Add(self.notes_list_widget, 1, wx.EXPAND)
+
+        self.title_widget = wx.TextCtrl(panel)
+        self.text_widget = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+
+        self.delete_btn = wx.Button(panel, label="Delete")
+        self.save_btn = wx.Button(panel, label="Save")
 
         info_btn = wx.Button(panel, label="Info")
         self.Bind(wx.EVT_BUTTON, self.on_info_btn_click, info_btn)
 
         buttons_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        buttons_sizer.Add(info_btn, 0, wx.LEFT)
-        buttons_sizer.Add(self.deleteBtn, 0, wx.ALIGN_LEFT)
-        buttons_sizer.Add(self.saveBtn, 0, wx.ALIGN_RIGHT)
+        buttons_sizer.Add(info_btn, 0, 0)
+        buttons_sizer.AddSpacer(5)
+        buttons_sizer.Add(self.delete_btn, 0, 0)
+        buttons_sizer.AddStretchSpacer()
+        buttons_sizer.Add(self.save_btn)
 
         edit_sizer = wx.BoxSizer(wx.VERTICAL)
-        edit_sizer.Add(self.textWidget, 1, wx.ALIGN_TOP | wx.EXPAND)
-        edit_sizer.Add(buttons_sizer, 0, wx.ALIGN_BOTTOM)
+        edit_sizer.Add(self.title_widget, 0, wx.EXPAND)
+        edit_sizer.Add(self.text_widget, 1, wx.ALIGN_TOP | wx.EXPAND | wx.TOP, 10)
+        edit_sizer.Add(buttons_sizer, 0, wx.EXPAND | wx.TOP, border=10)
 
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        main_sizer.Add(self.notesListWidget, 0, wx.ALIGN_LEFT)
-        main_sizer.Add(edit_sizer, 1, wx.ALIGN_RIGHT | wx.EXPAND)
+        main_sizer.Add(
+            notes_sizer, 2, wx.EXPAND | wx.TOP | wx.LEFT | wx.BOTTOM, border=15
+        )
+        main_sizer.Add(edit_sizer, 5, wx.ALIGN_RIGHT | wx.EXPAND | wx.ALL, border=15)
 
         panel.SetSizer(main_sizer)
 
